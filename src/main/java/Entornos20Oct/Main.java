282ebb4f00c2fdde95ce05e18c4f24d1ec435bc2
Joaquin <joaquinlc12@gmail.com>
2021-10-06 23:26:47 +0200

    primer commit

---
+++ src/main/java/Entornos20Oct/Main.java
@@ -0,0 +1,16 @@
+package Entornos20Oct;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("primera prueba");
+        System.out.println("cosa asoc");
+    }
+}
+
+
